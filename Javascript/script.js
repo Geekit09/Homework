f484a6a3f140c48950056ce18ebe1bf2854a88ff
Geekit09 <88097254+Geekit09@users.noreply.github.com>
2021-08-24 19:59:16 +0800

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,36 @@
+// 1.STRING
+//console.log(typeof 123546);
+
+// Хувьсагч - Variables
+// var huvisagch = true;
+// console.log(typeof huvisagch);
+
+// Converting DataType -
+// 1. toog string bolgoh
+
+// var nas = 29;
+// var strNas = nas.toString();
+
+// console.log(strNas);
+// console.log(nas);
+
+// 2. String datag too ruu horvuuleh
+// minii ner  => miniiNer salangad vg holboj bichij bga bol ehnii vg tom (Javascript deer)
+// parse integer (Number - buhel too)
+
+// var rating = "5";
+// var numRating = parseInt(5);
+// console.log(parseInt(rating));
+// console.log(numRating);
+
+// string, num hamt heregleh
+
+// var ner = "Narada";
+// var nas = 20;
+// console.log(
+//   "Сайн байна уу?... Миний нэр бол " + ner + ". Би " + nas + " настай. ");
+
+var myVar1 = "Сайн";
+var myVar2 = "Байна уу...";
+
+console.log(myVar1 + " " + myVar2);
