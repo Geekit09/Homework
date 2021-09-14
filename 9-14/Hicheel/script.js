295dc9af2912c20929718b4168d450e7ac4be263
Geekit09 <88097254+Geekit09@users.noreply.github.com>
2021-09-14 18:23:33 +0800

    Add files via upload

---
+++ 9-14/Hicheel/script.js
@@ -0,0 +1,173 @@
+// // Array methods
+// var arr = [5, 6, 7, 8, 9, 10];
+
+// // push - массивийн сүүлд элемент нэмж өгнө
+
+// arr.push(10, 11, 12, 13, 14);
+
+// // pop - Массивийн сүүлийн элемент нь устгана
+
+// arr.pop();
+// arr.pop();
+// arr.pop();
+
+
+// // unshift - массивийн эхэнд элмент нэмнэ
+// arr.unshift(2, 3, 4);
+
+// // shift() - массивийн эхний элементийг устгана.
+// arr.shift();
+
+// // reverse - Массивийг шууд урвуу байдалруу оруулна.
+// arr.reverse();
+// arr.pop();
+// //11, 10, 9, 8, 7, 6, 5, 4, 3
+// // splice - Массив дотроос дурын элементийг устгана.
+// arr.splice(3, 1);
+
+// // slice - arr-g tasdah awah
+
+// var x = arr.slice(4, 1)
+
+// // EcmaSCript
+// ES1, ES2, ES3 - EcmaScript
+// ES5 - 2009 - BUH browser DEMJINE / VAR /
+//     ES6 - 2015 - SHINE standart / LET /
+//         ES7 - 2016 -
+//         ES8 - 2017
+// ES9 - 2018
+// ES10 - 2019
+// ES11 - 2020
+// ES12 - 2021
+
+// SCOPE {}
+
+// let ner = "Geekit" // Global variable
+
+// {
+//     var x = 5; // Local Variable
+// }
+// console.log(x);
+
+// {
+//     let y = 6;
+//     console.log(ner);
+// }
+
+// Оролт: (5, 6, 0, 1, 2, 7) target=9;
+// Гаралт: [4,5];
+// let arr = [5, 6, 0, 1, 2, 7];
+// let target = 9;
+// for (let i = 0; i < arr.length; i++);
+// console.log(arr)
+// for (let j = i + 1; k < arr.length; j++) {
+//     if (arr[i] + arr[j] === target {
+//         XPathResult.push(i, j);
+//     )
+//     }
+
+// let result = [];
+// let arr = ["Ulaanbaater", 123, true, "Beijing", 258, "Tokyo", false, "Seoul"];
+// for (let i = 0; i < arr.length; i++) {
+//     if (typeof arr[i] === 'string') {
+//         result.push(i, j);
+//     }
+// }
+
+// Array - Data Structure
+// Object
+
+// let obj = {
+//       ner: "Narada",
+//         mergejil: "Designer"
+//     }
+//  
+
+// let obj1 = {
+//     ner: "Narada",
+//     mergejil: "Software Expert"
+// }
+// // [{}, {}, {},]
+// // let users = [{ner: "Gantulga", nas: 25}, {ner:"Nyamgerel"}]
+
+// let obj3 = [obj, obj1];
+
+// for (let i = 0; i < obj3.length; i++) {
+//     console.log(obj3[i]["ner"])
+// }
+
+// let email = prompt("Утгаа оруулна уу...")
+// let obj = {
+//     ner: "Narada",
+//     mergejil: "Designer",
+//     email: "blabla@gmail.com",
+//     cell: 654984489,
+// }
+
+// let obj2 = {
+//     ner: "Аз",
+//     mergejil: "Багш",
+//     email: "blabla2@gmail.com",
+//     cell: 654984489,
+// }
+// obj2.ner = "Азаа";
+// obj2.ger = 7 - 99;
+
+// let obj3 = {
+//     ner: "Үзмээ",
+//     mergejil: "Зураач",
+//     email: "blabla3@gmail.com",
+//     cell: 654984489,
+// }
+
+// let obj4 = {
+//     ner: "Naraa",
+//     mergejil: "Үүрэн холбооны ажилтан",
+//     email: "blabla4@gmail.com",
+//     cell: 654984489,
+// }
+
+// let obj5 = {
+//     ner: "Bold",
+//     mergejil: "Засварчин",
+//     email: "blabla5@gmail.com",
+//     cell: 1234569,
+// }
+
+// let obj6 = [obj, obj2, obj3, obj4, obj5];
+
+// for (let i = 0; i < obj6.length; i++) {
+//     if (obj6[i].email === email) {
+//         console.log(obj6[i])
+//     }
+// }
+
+// // for in , for of
+
+// for (usr of obj6) {
+//     if (usr.ner === ner) {
+//         console.log("ner");
+//     }
+// }
+
+
+// for (pro in obj6) {
+//     if (pro === "email") {
+
+//         console.log(obj6[pro]);
+//     }
+// }
+
+
+let one = 124,
+    let two = 48,
+
+    let Bat = {
+        name = "Bataa",
+        bills =[124, 48, 268],
+        tip =[],
+        finishbills =[],
+
+    }
+
+for (i = 0; i < )
