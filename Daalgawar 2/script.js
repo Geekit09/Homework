bcf412f8fb9cc3a547271cca8519eb9b4ab1c239
Geekit09 <88097254+Geekit09@users.noreply.github.com>
2021-09-21 00:45:05 +0800

    Add files via upload

--- Daalgawar 2/script.js
+++ Daalgawar 2/script.js
@@ -1,161 +1,528 @@
-// var i = "Gantulga";
+// // var i = "Gantulga";
 
-// for (var i = 0; i < ner.lenght; i++) {
-//    console.log(ner[i]);
+// // for (var i = 0; i < ner.lenght; i++) {
+// //    console.log(ner[i]);
+// // }
+
+// // Arrays - Data structure
+// //array - Массив
+
+// // var array = ["geekit", 23, true];
+// // console.log([array]);
+
+// // var ar = ["geekit", 25, false, true, 26];
+// // // // for (var i = 0; i < ar.length; i++) {
+// // // //    if (typeof array[i] === "string") {
+// // // //       console.log(ar[i]);
+// // // //    }
+// // // // }
+
+// // for (el of ar) {
+// //    if (typeof el === "number") {
+// //       console.log(el);
+// //    }
+// // }
+
+// // var ner = prompt("Утга оруулна уу...");
+// // var aWord = ["Amazing", "Adoroble", "Attractive", "Artful", "Adventurous"];
+// // var bWord = ["Brilliant", "Brave", "Bold", "Bright", "Baby"];
+// // var cWord = ["Cute", "Caring", "Courageous", "Charming", "Cool"];
+// // var dWord = ["Diplomatic", "Dynamic", "Decisive", "Depenable", "DeadMan"];
+// // var eWord = ["Efficient", "Excitable", "Energetic", "Emotional", "Entertaining"];
+// // var fWord = ["Friendly", "Fun", "Forceful", "Funny", "Fashionable"];
+// // var gWord = ["Glow", "Gem", "Giver", "Great", "GLACIER"];
+// // var hWord = ["Happy", "Handsome", "Healthy", "Helpful", "Hard-Working"];
+// // var iWord = ["Independent", "innocent", "Integrity", "Impatient", ""];
+// // var jWord = ["Joyful", "Joking", "Janitor", "Japanese", "Jackbooted"];
+// // var kWord = ["Kindly", "Kind", "Knight", "King", "Karma"];
+// // var lWord = ["Lucky", "LuckyGuy", "Luxurious", "Lovable", "LifeSaver"];
+// // var mWord = ["Major", "Model", "Master", "Mindful", "Motivated"];
+// // var nWord = ["Nice", "Nature", "Normal", "NERD", "Neighborly"];
+// // var oWord = ["Optimal", "One-Sided", "Outspoken", "Orderly", "Obnoxious"];
+// // var pWord = ["Pretty", "Provocative", "Pratical", "Powerful", "Passive"];
+// // var qWord = ["Quality", "Quantitative", "Quicker", "Questioning", "Queenly"];
+// // var rWord = ["Radiant", "Raging", "Regular", "Ruled", "Rushed"];
+// // var sWord = ["Sad", "SchoolBoy", "SchoolGirl", "Scientific", "Shy"];
+// // var tWord = ["Tiny", "Tall", "Triminator", "Trustworthy", "Thankful"];
+// // var uWord = ["Unique", "Unafraid", "UnAbashed", "UnBroken", "UnDefeted"];
+// // var vWord = ["Villain", "Violent", "Vulnerable", "Victorious", "Very"];
+// // var wWord = ["WarmHearted", "Wandered", "Warrior", "Wizard", "WonderWomen"];
+// // var xWord = ["Xany", "Xonked", "Xenodochial", "X-Rated", "Xenophobe"];
+// // var yWord = ["Young", "Yes-Man", "Yummy", "Yielding", "Yokozuna"];
+// // var zWord = ["Zealous", "Zombie", "Zoomer", "Zen", "Zippy"];
+
+// // for (var i = 0; i < ner.length; i++) {
+// //     var random = parseInt(Math.random() * gWord.length);
+// //     if (ner[i] === "a") {
+// //         console.log("A: " + aWord[random]);
+// //     } else if (ner[i] === "b") {
+// //         console.log("B: " + bWord[random]);
+// //     } else if (ner[i] === "c") {
+// //         console.log("C: " + cWord[random]);
+// //     } else if (ner[i] === "d") {
+// //         console.log("D: " + dWord[random]);
+// //     } else if (ner[i] === "e") {
+// //         console.log("E: " + eWord[random]);
+// //     } else if (ner[i] === "f") {
+// //         console.log("F: " + fWord[random]);
+// //     }  else if (ner[i] === "g") {
+// //         console.log("G: " + gWord[random]);
+// //     }
+// //     else if (ner[i] === "h") {
+// //         console.log("H: " + hWord[random]);
+// //     }
+// //     else if (ner[i] === "i") {
+// //         console.log("I: " + iWord[random]);
+// //     }
+// //     else if (ner[i] === "j") {
+// //         console.log("J: " + jWord[random]);
+// //     }
+// //     else if (ner[i] === "k") {
+// //         console.log("K: " + kWord[random]);
+// //     }
+// //     else if (ner[i] === "l") {
+// //         console.log("L: " + lWord[random]);
+// //     }
+// //     else if (ner[i] === "m") {
+// //         console.log("M: " + mWord[random]);
+// //     }
+// //     else if (ner[i] === "n") {
+// //         console.log("N: " + nWord[random]);
+// //     }
+// //     else if (ner[i] === "o") {
+// //         console.log("O: " + oWord[random]);
+// //     }
+// //     else if (ner[i] === "p") {
+// //         console.log("P: " + pWord[random]);
+// //     }
+// //     else if (ner[i] === "q") {
+// //         console.log("Q: " + qWord[random]);
+// //     }
+// //     else if (ner[i] === "r") {
+// //         console.log("R: " + rWord[random]);
+// //     }
+// //     else if (ner[i] === "s") {
+// //         console.log("S: " + sWord[random]);
+// //     }
+// //     else if (ner[i] === "t") {
+// //         console.log("T: " + tWord[random]);
+// //     }
+// //     else if (ner[i] === "u") {
+// //         console.log("U: " + uWord[random]);
+// //     }
+// //     else if (ner[i] === "v") {
+// //         console.log("V: " + vWord[random]);
+// //     }
+// //     else if (ner[i] === "w") {
+// //         console.log("W: " + wWord[random]);
+// //     }
+// //     else if (ner[i] === "x") {
+// //         console.log("X: " + xWord[random]);
+// //     }
+// //     else if (ner[i] === "y") {
+// //         console.log("Y: " + yWord[random]);
+// //     }
+// //     else if (ner[i] === "z") {
+// //         console.log("Z: " + zWord[random]);
+// //     }
+// // }
+
+// let bat = {
+//     ner: "Bat",
+//     tootsoo: [124, 48, 268],
+//     tip: [],
+//     finalBill: []
 // }
 
-// Arrays - Data structure
-//array - Массив
+// for(let i = 0; i < bat.tootsoo.length; i++){
+//     let percentage = 0;
+//     if(bat.tootsoo[i] <= 50){
+//         percentage = .2;
+//         bat.tip.push(bat.tootsoo[i] * percentage)
+//     } else if(bat.tootsoo[i] > 50 && bat.tootsoo[i] < 200){
+//         percentage = .15;
+//         bat.tip.push(bat.tootsoo[i] * percentage)
+//     } else {
+//         percentage = .1;
+//         bat.tip.push(bat.tootsoo[i] * percentage)
+//     }
 
-// var array = ["geekit", 23, true];
-// console.log([array]);
+//     let tip = 0;
+//     if(bat.tootsoo[i] <= 50){
+//         tip = 18.599999999999998;
+//         bat.finalBill.push(bat.tootsoo[i] + tip)
+//     } else if(bat.tootsoo[i] <=50){
+//         tip = 9.600000000000001;
+//         bat.finalBill.push(bat.tootsoo[i] + tip)
+//     } else {
+//         tip = 26.8;
+//         bat.finalBill.push(bat.tootsoo[i] + tip)
+//     }
+// } 
 
-// var ar = ["geekit", 25, false, true, 26];
-// // // for (var i = 0; i < ar.length; i++) {
-// // //    if (typeof array[i] === "string") {
-// // //       console.log(ar[i]);
-// // //    }
-// // // }
 
-// for (el of ar) {
-//    if (typeof el === "number") {
-//       console.log(el);
-//    }
+// // Array methods
+// var arr = [5, 6, 7, 8, 9, 10];
+
+// // push - массивийн сүүлд элемент нэмж өгнө
+
+// arr.push(10, 11, 12, 13, 14);
+
+// // pop - Массивийн сүүлийн элемент нь устгана
+
+// arr.pop();
+// arr.pop();
+// arr.pop();
+
+
+// // unshift - массивийн эхэнд элмент нэмнэ
+// arr.unshift(2, 3, 4);
+
+// // shift() - массивийн эхний элементийг устгана.
+// arr.shift();
+
+// // reverse - Массивийг шууд урвуу байдалруу оруулна.
+// arr.reverse();
+// arr.pop();
+// //11, 10, 9, 8, 7, 6, 5, 4, 3
+// // splice - Массив дотроос дурын элементийг устгана.
+// arr.splice(3, 1);
+
+// // slice - arr-g tasdah awah
+
+// var x = arr.slice(4, 1)
+
+// // EcmaSCript
+// ES1, ES2, ES3 - EcmaScript
+// ES5 - 2009 - BUH browser DEMJINE / VAR /
+//     ES6 - 2015 - SHINE standart / LET /
+//         ES7 - 2016 -
+//         ES8 - 2017
+// ES9 - 2018
+// ES10 - 2019
+// ES11 - 2020
+// ES12 - 2021
+
+// SCOPE {}
+
+// let ner = "Geekit" // Global variable
+
+// {
+//     var x = 5; // Local Variable
+// }
+// console.log(x);
+
+// {
+//     let y = 6;
+//     console.log(ner);
 // }
 
-// var ner = prompt("Утга оруулна уу...");
-// var aWord = ["Amazing", "Adoroble", "Attractive", "Artful", "Adventurous"];
-// var bWord = ["Brilliant", "Brave", "Bold", "Bright", "Baby"];
-// var cWord = ["Cute", "Caring", "Courageous", "Charming", "Cool"];
-// var dWord = ["Diplomatic", "Dynamic", "Decisive", "Depenable", "DeadMan"];
-// var eWord = ["Efficient", "Excitable", "Energetic", "Emotional", "Entertaining"];
-// var fWord = ["Friendly", "Fun", "Forceful", "Funny", "Fashionable"];
-// var gWord = ["Glow", "Gem", "Giver", "Great", "GLACIER"];
-// var hWord = ["Happy", "Handsome", "Healthy", "Helpful", "Hard-Working"];
-// var iWord = ["Independent", "innocent", "Integrity", "Impatient", ""];
-// var jWord = ["Joyful", "Joking", "Janitor", "Japanese", "Jackbooted"];
-// var kWord = ["Kindly", "Kind", "Knight", "King", "Karma"];
-// var lWord = ["Lucky", "LuckyGuy", "Luxurious", "Lovable", "LifeSaver"];
-// var mWord = ["Major", "Model", "Master", "Mindful", "Motivated"];
-// var nWord = ["Nice", "Nature", "Normal", "NERD", "Neighborly"];
-// var oWord = ["Optimal", "One-Sided", "Outspoken", "Orderly", "Obnoxious"];
-// var pWord = ["Pretty", "Provocative", "Pratical", "Powerful", "Passive"];
-// var qWord = ["Quality", "Quantitative", "Quicker", "Questioning", "Queenly"];
-// var rWord = ["Radiant", "Raging", "Regular", "Ruled", "Rushed"];
-// var sWord = ["Sad", "SchoolBoy", "SchoolGirl", "Scientific", "Shy"];
-// var tWord = ["Tiny", "Tall", "Triminator", "Trustworthy", "Thankful"];
-// var uWord = ["Unique", "Unafraid", "UnAbashed", "UnBroken", "UnDefeted"];
-// var vWord = ["Villain", "Violent", "Vulnerable", "Victorious", "Very"];
-// var wWord = ["WarmHearted", "Wandered", "Warrior", "Wizard", "WonderWomen"];
-// var xWord = ["Xany", "Xonked", "Xenodochial", "X-Rated", "Xenophobe"];
-// var yWord = ["Young", "Yes-Man", "Yummy", "Yielding", "Yokozuna"];
-// var zWord = ["Zealous", "Zombie", "Zoomer", "Zen", "Zippy"];
+// Оролт: (5, 6, 0, 1, 2, 7) target=9;
+// Гаралт: [4,5];
+// let arr = [5, 6, 0, 1, 2, 7];
+// let target = 9;
+// for (let i = 0; i < arr.length; i++);
+// console.log(arr)
+// for (let j = i + 1; k < arr.length; j++) {
+//     if (arr[i] + arr[j] === target {
+//         XPathResult.push(i, j);
+//     )
+//     }
 
-// for (var i = 0; i < ner.length; i++) {
-//     var random = parseInt(Math.random() * gWord.length);
-//     if (ner[i] === "a") {
-//         console.log("A: " + aWord[random]);
-//     } else if (ner[i] === "b") {
-//         console.log("B: " + bWord[random]);
-//     } else if (ner[i] === "c") {
-//         console.log("C: " + cWord[random]);
-//     } else if (ner[i] === "d") {
-//         console.log("D: " + dWord[random]);
-//     } else if (ner[i] === "e") {
-//         console.log("E: " + eWord[random]);
-//     } else if (ner[i] === "f") {
-//         console.log("F: " + fWord[random]);
-//     }  else if (ner[i] === "g") {
-//         console.log("G: " + gWord[random]);
-//     }
-//     else if (ner[i] === "h") {
-//         console.log("H: " + hWord[random]);
-//     }
-//     else if (ner[i] === "i") {
-//         console.log("I: " + iWord[random]);
-//     }
-//     else if (ner[i] === "j") {
-//         console.log("J: " + jWord[random]);
-//     }
-//     else if (ner[i] === "k") {
-//         console.log("K: " + kWord[random]);
-//     }
-//     else if (ner[i] === "l") {
-//         console.log("L: " + lWord[random]);
-//     }
-//     else if (ner[i] === "m") {
-//         console.log("M: " + mWord[random]);
-//     }
-//     else if (ner[i] === "n") {
-//         console.log("N: " + nWord[random]);
-//     }
-//     else if (ner[i] === "o") {
-//         console.log("O: " + oWord[random]);
-//     }
-//     else if (ner[i] === "p") {
-//         console.log("P: " + pWord[random]);
-//     }
-//     else if (ner[i] === "q") {
-//         console.log("Q: " + qWord[random]);
-//     }
-//     else if (ner[i] === "r") {
-//         console.log("R: " + rWord[random]);
-//     }
-//     else if (ner[i] === "s") {
-//         console.log("S: " + sWord[random]);
-//     }
-//     else if (ner[i] === "t") {
-//         console.log("T: " + tWord[random]);
-//     }
-//     else if (ner[i] === "u") {
-//         console.log("U: " + uWord[random]);
-//     }
-//     else if (ner[i] === "v") {
-//         console.log("V: " + vWord[random]);
-//     }
-//     else if (ner[i] === "w") {
-//         console.log("W: " + wWord[random]);
-//     }
-//     else if (ner[i] === "x") {
-//         console.log("X: " + xWord[random]);
-//     }
-//     else if (ner[i] === "y") {
-//         console.log("Y: " + yWord[random]);
-//     }
-//     else if (ner[i] === "z") {
-//         console.log("Z: " + zWord[random]);
+// let result = [];
+// let arr = ["Ulaanbaater", 123, true, "Beijing", 258, "Tokyo", false, "Seoul"];
+// for (let i = 0; i < arr.length; i++) {
+//     if (typeof arr[i] === 'string') {
+//         result.push(i, j);
 //     }
 // }
 
-let bat = {
-    ner: "Bat",
-    tootsoo: [124, 48, 268],
-    tip: [],
-    finalBill: []
-}
+// Array - Data Structure
+// Object
 
-for(let i = 0; i < bat.tootsoo.length; i++){
-    let percentage = 0;
-    if(bat.tootsoo[i] <= 50){
-        percentage = .2;
-        bat.tip.push(bat.tootsoo[i] * percentage)
-    } else if(bat.tootsoo[i] > 50 && bat.tootsoo[i] < 200){
-        percentage = .15;
-        bat.tip.push(bat.tootsoo[i] * percentage)
-    } else {
-        percentage = .1;
-        bat.tip.push(bat.tootsoo[i] * percentage)
+// let obj = {
+//       ner: "Narada",
+//         mergejil: "Designer"
+//     }
+//  
+
+// let obj1 = {
+//     ner: "Narada",
+//     mergejil: "Software Expert"
+// }
+// // [{}, {}, {},]
+// // let users = [{ner: "Gantulga", nas: 25}, {ner:"Nyamgerel"}]
+
+// let obj3 = [obj, obj1];
+
+// for (let i = 0; i < obj3.length; i++) {
+//     console.log(obj3[i]["ner"])
+// }
+
+// 
+// let obj = {
+//     ner: "Narada",
+//     mergejil: "Designer",
+//     email: "blabla@gmail.com",
+//     cell: 654984489,
+// }
+
+// let obj2 = {
+//     ner: "Аз",
+//     mergejil: "Багш",
+//     email: "blabla2@gmail.com",
+//     cell: 654984489,
+// }
+// obj2.ner = "Азаа";
+// obj2.ger = 7 - 99;
+
+// let obj3 = {
+//     ner: "Үзмээ",
+//     mergejil: "Зураач",
+//     email: "blabla3@gmail.com",
+//     cell: 654984489,
+// }
+
+// let obj4 = {
+//     ner: "Naraa",
+//     mergejil: "Үүрэн холбооны ажилтан",
+//     email: "blabla4@gmail.com",
+//     cell: 654984489,
+// }
+
+// let obj5 = {
+//     ner: "Bold",
+//     mergejil: "Засварчин",
+//     email: "blabla5@gmail.com",
+//     cell: 1234569,
+// }
+
+// let obj6 = [obj, obj2, obj3, obj4, obj5];
+
+// for (let i = 0; i < obj6.length; i++) {
+//     if (obj6[i].email === email) {
+//         console.log(obj6[i])
+//     }
+// }
+
+// // for in , for of
+
+// for (usr of obj6) {
+//     if (usr.ner === ner) {
+//         console.log("ner");
+//     }
+// }
+
+
+// for (pro in obj6) {
+//     if (pro === "email") {
+
+//         console.log(obj6[pro]);
+//     }
+// }
+
+// Primitive Data type
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. undefined
+// 5. null
+
+// Object, Array -Non primitive
+// function - Функц гэдэн нь аливаа программийг бүрдүүлж, бүрэлдэхүүн хэсэг.
+
+
+// function myfunc() {
+//     let tursunOn = 2000;
+//     let nas = 2021 - 2000;
+//     return nas;
+// }
+// let x = myFunc();
+
+// function nasTootsoh(on) {
+//     return 2021 - оn;
+
+
+//     let person = nasTootsoh(2000);
+//     let person = nasTootsoh(2001;
+//     let person = nasTootsoh(1997);
+//     let person = nasTootsoh(1995);
+
+
+// Оролт - 1, 0, 5; Гаралт : 1
+// Оролт 5, 3, 1; Гаралт: 0
+
+
+// function myfirst(too1, too2, too3) {
+//     let teg = 0;
+//     if (too1 === 0) {
+//         teg++;
+
+//     }
+//     if (too2 === 0) {
+//         teg++;
+
+//     }
+//     if (too3 === 0) {
+//         teg++;
+
+//     }
+
+//     return teg;
+
+
+// }
+
+
+// function myfirst(a, b) {
+//     return a ** 3;
+// }
+
+
+// function myfirst(a) {
+//     return a.toString().length;
+// }
+
+// function myfirst(a) {
+//     let d = 0;
+//     for (let i = 0; i < a.toString().length; i++)
+//         d += +a.toString()[i]
+//     return d;
+// }
+
+// function myfirst(a) {
+//     let noob = 0;
+//     for (let i = 0; i < a.toString().length; i++) {
+//         if (a.toString()[i] % 2 === 0) {
+//             noob++;
+//         }
+
+//     }
+
+//     return noob;
+// }
+
+
+// function myfirst(a) {
+//     let noob = 0;
+//     for (let i = 0; i < a.toString().length; i++) {
+//         if (a.toString()[i] % 2 === 0) {
+//             noob += +a.toString()[i];
+
+//         }
+//     }
+//     return noob;
+// }
+
+
+
+// function noob(a) {
+//     return a === +a.toString().split("").reverse().join("");
+// }
+
+// function noob(a) {
+//     let = noob
+//     for (let i = 0; i < a; i++) {
+//         if (a.toString().length / )
+
+//     }
+// }
+
+
+//CallStack - Нэг функцыг өөр функцэд дуудахыг
+
+
+
+//Callback - аргумент хэлбэрээр функц дамжуулах
+
+// Recursion - функцыг өөрийг нь дотор нь дуудах
+
+
+// function factorial(a) {
+//     if (a === 1) {
+
+//         return a;
+//     }
+//     return a * factorial(a - 1)
+// }
+
+//Homework
+
+// let base = Number(prompt("Суурь утга оруулна уу..."));
+// let height = Number(prompt("Өндөр утга оруулна уу..."));
+
+// function myfirst(a, b){
+//     let triArea =  (base * height) / 2;
+
+//     return ("Талбайн хэмжээ: " + triArea);
+// }
+
+
+
+
+
+// let obj = [
+//     {
+//        obj1: true
+//     },
+//     {
+//        obj1: false
+//     },
+//     {
+//        obj1: true
+//     },
+//     {
+//        obj1: true
+//     },
+//     {
+//        obj1: false
+//     }
+//  ]
+//  function counTrue(obj) {
+//     let counter = 0;
+//     for (let i = 0; i < obj.length; i++) {
+//        if (obj[i].obj1 === true) {
+//           counter++;
+//        }
+//     }
+//     return counter;
+//  }
+//  function countFalse(obj) {
+//     let counter = 0;
+//     for (let i = 0; i < obj.length; i++) {
+//        if (obj[i].obj1 === false) {
+//           counter++;
+//        }
+//     }
+//     return counter;
+//  }
+//  console.log("Үнэн гэсэн утга: " + counTrue(obj));
+//  console.log("Худал гэсэн утга: " + countFalse(obj));
+
+
+let obj = [
+    {
+        a: 1
+    },
+    {
+        b: 2
+    },
+    {
+        c: 3
     }
+]
 
-    let tip = 0;
-    if(bat.tootsoo[i] <= 50){
-        tip = 18.599999999999998;
-        bat.finalBill.push(bat.tootsoo[i] + tip)
-    } else if(bat.tootsoo[i] <=50){
-        tip = 9.600000000000001;
-        bat.finalBill.push(bat.tootsoo[i] + tip)
-    } else {
-        tip = 26.8;
-        bat.finalBill.push(bat.tootsoo[i] + tip)
-    }
-} 
+function counTrue(obj) {
+        let counter = 0;
+        for (let i = 0; i < obj.length; i++) {
+           if (obj[key[x]] = val[x]) {
+              counter++;
+           }
+        }
+        return counter;
+     }
+     
+console.log (key ["a", "b", "c"]);
+// // for (let x = 0; x < key.length; x++){
+// //     obj[key[x]] = val[x];
+// }
